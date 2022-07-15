a284792a9237aa27c8f5db4786f467fa7f77a998
carpe <carpeas@outlook.es>
2022-07-15 16:17:38 +0200

    segundaFila

--- calculadoraPython.py
+++ calculadoraPython.py
@@ -1,3 +1,4 @@
+from codecs import BufferedIncrementalEncoder
 from tkinter import *
 from turtle import right
 
@@ -7,9 +8,17 @@ miFrame=Frame(raiz)
 miFrame.pack()
 #---------------pantalla-----------------
 pantalla=Entry(miFrame)
-pantalla.grid(row=1,column=1,padx=10,pady=11)
-pantalla.config(bg="black",fg="green",justify="right")#03f943
+pantalla.grid(row=1,column=1,padx=5,pady=5,columnspan=4)
+pantalla.config(bg="black",fg="#03f943",justify="right")#03f943
 
-
+#---------------fila1-----------------
+boton7=Button(miFrame,text="7",width=3)
+boton7.grid(row=2,column=1)
+boton8=Button(miFrame,text="8",width=3)
+boton8.grid(row=2,column=2)
+boton9=Button(miFrame,text="9",width=3)
+boton9.grid(row=2,column=3)
+botonDiv=Button(miFrame,text="/",width=3)
+botonDiv.grid(row=2,column=4)
 
 raiz.mainloop()
