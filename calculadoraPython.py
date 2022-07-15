370a3439886cb2d9a5f2de5d26737574f2cc6213
carpe <carpeas@outlook.es>
2022-07-15 17:48:13 +0200

    funcionSuma

--- calculadoraPython.py
+++ calculadoraPython.py
@@ -8,9 +8,11 @@ raiz=Tk()
 miFrame=Frame(raiz)
 miFrame.pack(fill='both')#, expand=False
 miFrame.config(cursor='hand2')
-numeroPantalla=StringVar()
+operacion=""
+resultado=0
 
 #---------------pantalla-----------------
+numeroPantalla=StringVar()
 pantalla=Entry(miFrame,textvariable=numeroPantalla)
 pantalla.grid(row=1,column=1,padx=5,pady=5,columnspan=4)
 pantalla.config(bg="black",fg="#03f943",justify="right")#03f943
@@ -18,13 +20,35 @@ pantalla.config(bg="black",fg="#03f943",justify="right")#03f943
 #---------------pulsacionesPantalla-----------------
 
 def numeroPulsado(num):
-    numeroPantalla.set(numeroPantalla.get()+num)
+    global operacion
+    if operacion!="":
+        numeroPantalla.set(num)
+        operacion=""
+    else:
+        numeroPantalla.set(numeroPantalla.get()+num)
    
+#---------------funcionSuma-----------------
+def suma(num):
+    global operacion
+    global resultado
+
+    resultado+=int(num)#float
+    operacion="suma"
+
+    numeroPantalla.set(resultado)
+
+#---------------funcionTotal-----------------
+
+def total():
+    global resultado
+
+    numeroPantalla.set(resultado+int(numeroPantalla.get()))
+
+    resultado=0
 
 #---------------fila1-----------------
 boton7=Button(miFrame,text="7",width=3,command=lambda:numeroPulsado("7"))
 boton7.grid(row=2,column=1)
-#boton7=Entry(miFrame,textvariable="7")
 boton8=Button(miFrame,text="8",width=3,command=lambda:numeroPulsado("8"))
 boton8.grid(row=2,column=2)
 boton6=Button(miFrame,text="9",width=3,command=lambda:numeroPulsado("9"))
@@ -57,9 +81,9 @@ boton0=Button(miFrame,text="0",width=3,command=lambda:numeroPulsado("0"))
 boton0.grid(row=5,column=1)
 botonCom=Button(miFrame,text=",",width=3,command=lambda:numeroPulsado(","))
 botonCom.grid(row=5,column=2)
-botonEqual=Button(miFrame,text="=",width=3)
+botonEqual=Button(miFrame,text="=",width=3,command=lambda:total())
 botonEqual.grid(row=5,column=3)
-botonSum=Button(miFrame,text="+",width=3)
+botonSum=Button(miFrame,text="+",width=3,command=lambda:suma(numeroPantalla.get()))
 botonSum.grid(row=5,column=4)
 
 
