cc31f9ec98e8f739eb1ae7337a6755f01042e737
carpe <carpeas@outlook.es>
2022-07-15 16:03:05 +0200

    pantalla

--- calculadoraPython.py
+++ calculadoraPython.py
@@ -1,8 +1,15 @@
 from tkinter import *
+from turtle import right
 
 raiz=Tk()
 
 miFrame=Frame(raiz)
 miFrame.pack()
+#---------------pantalla-----------------
+pantalla=Entry(miFrame)
+pantalla.grid(row=1,column=1,padx=10,pady=11)
+pantalla.config(bg="black",fg="green",justify="right")#03f943
+
+
 
 raiz.mainloop()
